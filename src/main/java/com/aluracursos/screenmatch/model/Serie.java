5a3cdf6c18e3d7b2a98e177ee1282218c5e8feb1
developerharo <developerharo@gmail.com>
2025-07-24 16:07:31 -0600

    Mapeando las entidades

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -1,15 +1,22 @@
 package com.aluracursos.screenmatch.model;
-
 import com.aluracursos.screenmatch.service.ConsultaChatGPT;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double evaluacion;
     private String poster;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String actores;
     private String sinopsis;
@@ -34,6 +41,14 @@ public class Serie {
                 ", poster='" + poster + '\'' +
                 ", actores='" + actores + '\'' +
                 ", sinopsis='" + sinopsis + '\'';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
